ec67cafecdb2d635fbdb94605d62f4a1d4d80d22
weibo <919006380@qq.com>
2018-11-10 22:38:43 +0800

    完善博客

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -7,57 +7,66 @@ module.exports = {
         // activeHeaderLinks: false, // 活动的标题链接 默认值：true 
         // displayAllHeaders: true, // 显示所有页面的标题链接 默认值：false
       nav: [
-        { text: 'Home', link: '/' },
+        { text: 'Home', link: '/' },//主页
         { text: 'Blog', link: '/blog/' },
         { text: 'Notes', link: '/notes/' },
-        { text: 'About', link: 'About' }, 
-        { text: 'github', link: 'https://github.com' },       
+        { text: 'About', link: 'about/about' }, 
+
       ],
       sidebar: {
+        // '/': [// 主页
+        //   '',        
+        //   ['主页','主页'],
+        // ],
         '/blog/': [
-            '',
-            {
-                title: 'Group 1',
-                collapsable: false,
-                children: [
-                    // '',
-                    ['four','four'],
-                    ['three','three']
-                ]
-            },
-            {
-                title: 'Group 2',
-                children: [
-                    // '',
-                    ['ng','ng_title']
-                ]
-            }
+          '', //README
+          ['angular1.x使用','angular1.x的使用记录'],
+          ['three','three'],
+          ['ng','ng_title'],
+          ['闭包','闭包']
         ],
         '/notes/': [
           '',      
-        //   'Angular1.x.md',
-        //   'Angular6.md',
-          ['js设计模式.md','js设计模式'],
-          ['Promise.md','Promise'],
-          ['webpack.md','webpack'],
-          ['书籍.md','书籍'],
-          ['js深入浅出.md','js深入浅出'],
-          ['造轮子思路.md','造轮子思路'],
-          ['ES6.md','ES6'],
-          ['Nodejs.md','Nodejs'],
-          ['typescript.md','typescript'],
-          ['编程概念.md','编程概念'],
-          ['JS概念.md','JS概念'],
-          ['vue.md','vue'],
-          ['前端资源.md','前端资源'],
-          
-        ],
-        // fallback
-        '/': [
-          '',        
-          ['contact','主页'],
-          ['About','关于']
+          {
+            title: '笔记',
+            // collapsable: false,//侧边栏自动收缩展开
+            children: [
+                // '',
+                //   'Angular1.x.md',
+                //   'Angular6.md',
+                ['js设计模式.md','js设计模式'],
+                ['Promise.md','Promise'],
+                ['webpack.md','webpack'],
+                ['js深入浅出.md','js深入浅出'],
+                ['造轮子思路.md','造轮子思路'],
+                ['ES6.md','ES6'],
+                ['Nodejs.md','Nodejs'],
+                ['typescript.md','typescript'],
+                ['编程概念.md','编程概念'],
+                ['JS概念.md','JS概念'],
+                ['vue.md','vue'],
+                ['web前端/实用代码段.md','代码段'],
+                // ['web前端/ajax&fetch&跨域.md','ajax&fetch&跨域'],
+                ['web前端/git笔记.md','git笔记'],
+                ['web前端/浏览器兼容问题.md','浏览器兼容问题'],
+                ['web前端/DOM和事件.md','DOM和事件'],
+                // ['web前端/HTML5+CSS3.md','HTML5+CSS3'],
+                ['web前端/实用代码段.md','实用代码段'],
+                // ['web前端/flex布局&Grid 布局.md','flex布局&Grid 布局'],
+                ['web前端/JavaScript.md','JavaScript'],
+
+            ]
+          },
+          {
+              title: '资源',
+              children: [
+                  // '',
+                  ['前端资源.md','前端资源'],
+                  ['书籍.md','书籍'],
+              ]
+          } 
         ]
+        
       }
     
     }
