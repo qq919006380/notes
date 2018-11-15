5fc1e55ce2335ed1512ae51d57a0ff5615a20f77
qq919006380 <919006380@qq.com>
2018-11-15 13:44:34 +0800

    add

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     title: '夏天夏',  // 设置网站标题
     dest: './dist',    // 设置输出目录
-    base: '/root/', // 设置站点根路径
+    base: '/notes/', // 设置站点根路径
     repo: 'https://github.com/qq919006380/notes', // 添加 github 链接
     themeConfig: {
         // activeHeaderLinks: false, // 活动的标题链接 默认值：true 
