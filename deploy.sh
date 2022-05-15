818cbf24d3e28d0c0b713a7699f80b3287fee0ad
weibo <919006380@qq.com>
2022-05-15 17:45:20 +0800

    init

--- deploy.sh
+++ deploy.sh
@@ -4,23 +4,40 @@
 set -e
 
 # 生成静态文件
-npm run docs:build
+npm run build
 
 # 进入生成的文件夹
 cd docs/.vuepress/dist
 
-# 如果是发布到自定义域名
-echo 'weibaichao.com' > CNAME
+# deploy to github pages
+echo 'www.weibaichao.com/' > CNAME
 
+if [ -z "$GITHUB_TOKEN" ]; then
+  msg='deploy'
+  githubUrl=git@github.com:qq919006380/notes.git
+else
+  msg='来自github actions的自动部署'
+  githubUrl=https://xugaoyi:${GITHUB_TOKEN}@github.com/qq919006380/notes.git
+  git config --global user.name "qq919006380"
+  git config --global user.email "919006380@qq.com"
+fi
 git init
 git add -A
-git commit -m 'deploy.sh'
+git commit -m "${msg}"
+git push -f $githubUrl master:gh-pages # 推送到github gh-pages分支
 
-# 如果发布到 https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+# deploy to coding pages
+# echo 'www.xugaoyi.com\nxugaoyi.com' > CNAME  # 自定义域名
+# echo 'google.com, pub-7828333725993554, DIRECT, f08c47fec0942fa0' > ads.txt # 谷歌广告相关文件
 
-# 如果发布到 https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
-git push -f git@github.com:qq919006380/notes.git master:gh-pages
+# if [ -z "$CODING_TOKEN" ]; then  # -z 字符串 长度为0则为true；$CODING_TOKEN来自于github仓库`Settings/Secrets`设置的私密环境变量
+#   codingUrl=git@e.coding.net:xgy/xgy.git
+# else
+#   codingUrl=https://HmuzsGrGQX:${CODING_TOKEN}@e.coding.net/xgy/xgy.git
+# fi
+# git add -A
+# git commit -m "${msg}"
+# git push -f $codingUrl master # 推送到coding
 
 cd -
+rm -rf docs/.vuepress/dist
--- docs/blog/node服务端.md
+++
@@ -1,6 +0,0 @@
-热重启
-- npm install nodemon --save
-
-koa中常用的语法
-- promise
-- Async、Await
