0b73f2c21c9c62e69a8b527c985971b785a1d6da
weibo <919006380@qq.com>
2022-05-15 23:12:40 +0800

    ad

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ npm run build
 cd docs/.vuepress/dist
 
 # deploy to github pages
-echo 'www.weibaichao.com/' > CNAME
+echo 'www.weibaichao.com' > CNAME
 
 if [ -z "$GITHUB_TOKEN" ]; then
   msg='deploy'
