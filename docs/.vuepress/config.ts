90b45b379b079ae86d007ef97e397a6a0713fdb3
weibo <919006380@qq.com>
2022-05-18 00:20:15 +0800

    add

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -7,6 +7,7 @@ import { VdoingThemeConfig } from "vuepress-theme-vdoing/types";
 import dayjs from "dayjs";
 import baiduCode from "./config/baiduCode"; // 百度统计hm码
 import htmlModules from "./config/htmlModules"; // 自定义插入的html块
+import { readFileList, readTotalFileWords, readEachFileWords } from './webSiteInfo/readFile';
 
 export default defineConfig4CustomTheme<VdoingThemeConfig>({
   theme: "vdoing", // 使用npm主题包
@@ -40,6 +41,20 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
       bubble: false, // 是否开启图片的气泡效果，默认为 false
       bubblePosition: 0, // 气泡效果的位置，范围：0-100，不同数值代表不同的起始位置，0是整个图片，50是半张图（一半的下方）。bubble 为 true 生效。默认是 0
       bubbleNum: 200, // 气泡的个数，bubble 为 true 生效，默认 200 个
+    },
+    blogInfo: {
+      blogCreate: '2019-12-12', // 博客创建时间
+      indexView: true,  // 开启首页的访问量和排名统计，默认 true（开启）
+      pageView: false,  // 开启文章页的浏览量统计，默认 true（开启）
+      readingTime: true,  // 开启文章页的预计阅读时间，条件：开启 eachFileWords，默认 true（开启）。可在 eachFileWords 的 readEachFileWords 的第二个和第三个参数自定义，默认 1 分钟 300 中文、160 英文
+      eachFileWords: readEachFileWords([''], 300, 160),  // 开启每个文章页的字数。readEachFileWords(['xx']) 关闭 xx 目录（可多个，可不传参数）下的文章页字数和阅读时长，后面两个参数分别是 1 分钟里能阅读的中文字数和英文字数。无默认值。readEachFileWords() 方法默认排除了 article 为 false 的文章
+      mdFileCountType: 'archives',  // 开启文档数。1. archives 获取归档的文档数（默认）。2. 数组 readFileList(['xx']) 排除 xx 目录（可多个，可不传参数），获取其他目录的文档数。提示：readFileList() 获取 docs 下所有的 md 文档（除了 `.vuepress` 和 `@pages` 目录下的文档）
+      totalWords: 'archives',  // 开启本站文档总字数。1. archives 获取归档的文档数（使用 archives 条件：传入 eachFileWords，否则报错）。2. readTotalFileWords(['xx']) 排除 xx 目录（可多个，可不传参数），获取其他目录的文章字数。无默认值
+      moutedEvent: '.tags-wrapper',   // 首页的站点模块挂载在某个元素后面（支持多种选择器），指的是挂载在哪个兄弟元素的后面，默认是热门标签 '.tags-wrapper' 下面，提示：'.categories-wrapper' 会挂载在文章分类下面。'.blogger-wrapper' 会挂载在博客头像模块下面
+      // 下面两个选项：第一次获取访问量失败后的迭代时间
+      indexIteration: 2500,   // 如果首页获取访问量失败，则每隔多少时间后获取一次访问量，直到获取成功或获取 10 次后。默认 3 秒。注意：设置时间太低，可能导致访问量 + 2、+ 3 ......
+      pageIteration: 2500,    // 如果文章页获取访问量失败，则每隔多少时间后获取一次访问量，直到获取成功或获取 10 次后。默认 3 秒。注意：设置时间太低，可能导致访问量 + 2、+ 3 ......
+      // 说明：成功获取一次访问量，访问量 + 1，所以第一次获取失败后，设置的每个隔段重新获取时间，将会影响访问量的次数。如 100 可能每次获取访问量 + 3
     },
     // 导航配置
     nav: [
@@ -197,6 +212,8 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
   // 注入到页面<head>中的标签，格式[tagName, { attrName: attrValue }, innerHTML?]
   head: [
     ["link", { rel: "icon", href: "/img/a.jpg" }], //favicons，资源放在public文件夹
+    ["link", { rel: "stylesheet", href: "https://at.alicdn.com/t/font_3077305_pt8umhrn4k9.css" }],
+
     [
       "meta",
       {
@@ -206,6 +223,9 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
     ],
     ["meta", { name: "baidu-site-verification", content: "7F55weZDDc" }], // 百度统计的站长验证（你可以去掉）
     ["meta", { name: "theme-color", content: "#11a8cd" }], // 移动浏览器主题颜色
+    ["meta", { name: "referrer", content: "no-referrer-when-downgrade" }],
+    ["link", { rel: "stylesheet", href: "https://at.alicdn.com/t/font_3077305_pt8umhrn4k9.css" }],
+
     // [
     //   'script',
     //   {
@@ -219,7 +239,7 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
   // 插件配置
   plugins: <UserPlugins>[
     "vuepress-plugin-baidu-autopush", // 百度自动推送
-
+    "reading-progress",
     [
       "vuepress-plugin-baidu-tongji", // 百度统计
       {
